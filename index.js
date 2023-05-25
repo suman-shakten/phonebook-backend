acab2b01d32b7f97facbed021594ae2bdddb2b65
AsRoAS <saorsa97@gmail.com>
2023-05-25 22:36:55 +0545

    finished exercise 3.7

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 
-app.use(express.json());
+const app = express();
+app.use(morgan('tiny'));
 
 let persons = [
     {
