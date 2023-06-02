3c1f91635196d21c102119a04594ebac26dd95aa
AsRoAS <saorsa97@gmail.com>
2023-06-02 18:52:20 +0545

    finished exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 morgan.token('body', request => {
     return JSON.stringify(request.body);
@@ -8,6 +9,7 @@ morgan.token('body', request => {
 const app = express();
 app.use(express.json());
 app.use(morgan(':method :url :status :response-time ms :body'));
+app.use(cors());
 
 let persons = [
     {
