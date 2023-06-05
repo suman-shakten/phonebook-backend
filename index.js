d04b241aeeec4548fdd93ffcc07b98226b342a76
AsRoAS <saorsa97@gmail.com>
2023-06-05 12:13:38 +0545

    finished exercise 3.13

--- index.js
+++ index.js
@@ -1,107 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+require('dotenv').config();
 
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('build'));
+
+const morgan = require('morgan');
 morgan.token('body', request => {
     return JSON.stringify(request.body);
 });
-
-const app = express();
-app.use(express.json());
 app.use(morgan(':method :url :status :response-time ms :body'));
+
+const cors = require('cors');
 app.use(cors());
-app.use(express.static('build'));
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map((person) => person.id))
-        : 0;
-    return maxId + 1;
-}
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'Name or number is missing'
-        });
-    };
-
-    const duplicatePerson = persons.find((person) => person.name === body.name);
-    if (duplicatePerson) {
-        return response.status(400).json({
-            error: 'Name must be unique'
-        });
-    };
-
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    };
-
-    persons = persons.concat(person);
-    response.json(person);
-})
-
-app.get('/', (request, response) => {
-    response.send('<h1>Phonebook-Backend</h1>');
-});
+const mongoose = require('mongoose');
+const Person = require('./models/person');
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
-app.get('/info', (request, response) => {
-    response.send(`<div>
-    <p>Phonebook has info for ${persons.length} people.</p>
-     <p>${new Date()}</p>
-     </div>`);
-});
-
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id)
-
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
-
-    response.status(204).end();
-});
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
