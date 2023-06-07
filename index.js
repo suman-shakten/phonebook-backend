89d324a80cc5650d389e65bfc5b0100e4b268f14
AsRoAS <saorsa97@gmail.com>
2023-06-07 13:45:59 +0545

    finished exercise 3.19

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
+
     next(error);
 }
 
@@ -43,7 +46,7 @@ app.get('/api/persons', (request, response) => {
     });
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -57,9 +60,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     });
 
-    person.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    person.save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch((error) => next(error))
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
